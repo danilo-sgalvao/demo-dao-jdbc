4b751670e97d0042bda8f5ec10178e59ca9a83cb
danilo-sgalvao <danilo.galvao@discente.ufg.br>
2025-11-07 14:45:48 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -32,6 +33,11 @@ public class Program {
 		list= sellerDao.findAll();
 		list.forEach(System.out::println);
 		
+		
+		System.out.println("\n=== TEST 4: seller insert ===");
+		Seller addSeller = new Seller(null, "Greg", "greg@gmail", LocalDate.now(), 4000.00, department);
+		sellerDao.insert(addSeller);
+		System.out.println("Inserted! New id = " + addSeller.getid());
 	}
 
 }
